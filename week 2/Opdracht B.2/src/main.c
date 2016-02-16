ba2b7b7514eae68231bf990a62b7118d2f6ebf6c
rene1997 <renekeijzer1997@gmail.com>
2016-02-16 11:58:11 +0100

    opdracht B2

--- week 2/Opdracht B.2/src/main.c
+++ week 2/Opdracht B.2/src/main.c
@@ -29,12 +29,50 @@
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 #include <asf.h>
+#include <util/delay.h>
+#include "main.h"
+ 
+int x = 0;
 
 int main (void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
-
+	
 	board_init();
+	DDRD = 0b11110000;			//set pin 0 - 4 input 
+								//set pin 4 - 7 output
+	DDRC = 0b11111111;			//set full port c output
+	//Init Interrupt hardware
+	EICRA |= 0b00111100;		//int 1 rising edge, int 2 rising edge
+	EIMSK |= 0b00000110;		//enable INT 1 & INT 2
+	sei();						//sets global interrupt enable (page 16 documentation)
 
-	/* Insert application code here, after the board has been initialized. */
+	while(1){
+		//empty!
+		wait(1);
+	}
+	return 1;
 }
+void loopLight(){
+	PORTC = (1<<x);
+	x += 1;
+	if(x > 7){
+		x = 0;
+	}
+}
+
+ISR(INT2_vect){
+	loopLight();
+}
+
+ISR(INT1_vect){
+	loopLight();
+}
+
+
+void wait(int milliseconds){
+	for(int i = 0; i < milliseconds; i++){
+	_delay_ms(1);
+	}
+}
+
+
---
+++ week 2/Opdracht B.2/src/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ * Created: 16-2-2016 11:31:59
+ *  Author: rene
+ */ 
+
+ #pragma once
+
+ void wait(int milliseconds);
+ ISR( INT1_vect );
+ ISR ( INT2_vect);
+
+
