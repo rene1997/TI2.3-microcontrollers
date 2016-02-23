c410abd1e9b2bb7368f4859d56b5508ef580260c
Arjen Mourik <arjen.mourik@live.nl>
2016-02-23 12:35:45 +0100

    -

--- week 3/week 3/src/main.c
+++ week 3/week 3/src/main.c
@@ -37,11 +37,17 @@
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
-
+	char c[] = "Test 1234567 TEST";
 	board_init();
 	init_lcd();
 
-	lcd_writeChar('A');
+	while(1)
+	{
+		//lcd_writeChar('A');
+		lcd_writeLine1(c);
+		_delay_ms(5000);
+	}
+	
 
 
 
@@ -50,10 +56,15 @@ int main (void)
 
 void init_lcd(){
 	//return home8
+	DDRC = 0b11111111;
+	_delay_ms(100);
 	lcd_command(0x02);
 
 	//mode: 4 bits interface data, 2 lines, 5x data
 	lcd_command(0x28);
+
+	// display: on, cursor off, blinking off 
+	lcd_command( 0x0C ); 
 
 	//entry mode: cursor to right no shift
 	lcd_command(0x06);
@@ -78,22 +89,24 @@ void lcd_command(unsigned char dat){
 void lcd_writeChar( unsigned char dat)
 {
 	PORTC = dat & 0xF0;
-	//hogenibble
 	PORTC = PORTC | 0x0C;
-	// data (RS=1),
-	// start (EN=1)
+
 	_delay_ms(1);
-	// wait 1 ms
 	PORTC = 0x04;
-	// stop (EN = 0)
-	PORTC = (dat& 0x0F) << 4;
-	//lagenibble
+
+	PORTC = (dat & 0x0F) << 4;
 	PORTC = PORTC | 0x0C;
-	// data (RS=1),
-	// start (EN=1)
+
 	_delay_ms(1);
-	// wait 1 ms
 	PORTC = 0x00;
-	//stop
-	// (EN=0 RS=0)
+}
+
+void lcd_writeLine1 ( char text1[] )
+{
+	// eerst de eerste 8 karakters = regel 1
+	// eerste pos regel 1
+	lcd_command(0x80);
+	for (int i=0; i<16; i++) {
+		lcd_writeChar( text1[i] );
+	}
 }
