753a04f1847bcc3e3050d7372aa6f2e00755303f
rene1997 <renekeijzer1997@gmail.com>
2016-02-23 11:40:52 +0100

    start opdrachten week 3

---
+++ week 3/week 3/src/main.h
@@ -0,0 +1,18 @@
+/*
+ * main.h
+ *
+ * Created: 23-2-2016 11:28:29
+ *  Author: rkeet
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void init_lcd();
+void lcd_command(unsigned char);
+void lcd_writeChar( unsigned char);
+
+
+
+#endif /* MAIN_H_ */
