9e902f613b2bd9692b581a15b4860004450641c4
rene1997 <renekeijzer1997@gmail.com>
2016-02-29 16:26:15 +0100

    last changes opdracht b2

--- week 3/Opdracht B.2/src/main.c
+++ week 3/Opdracht B.2/src/main.c
@@ -37,34 +37,45 @@
 #define BIT(x) (1<<(x))	
 
 char numbers[4];
-int amountpressed = 0;
+int amountpressed = 1;
 
 ISR( TIMER2_OVF_vect )
 {
+	//set in char array numbers amountpressed
 	sprintf(numbers,"%d",amountpressed);
+
+-	//write array lcd screen
 	lcd_writeLine(numbers, 1);
+	
+	//add 1 t0 amountpressed
 	amountpressed ++;
 
+	//set cursorPosition to 0
 	lcd_setCursorPosition(0);
+
+	//set counter -1, when one time pressed, overflow event toggles (page 146 documentation)
 	TCNT2 = -1;
 }
 
 int main (void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
-
+	//start lcd screen
 	init_lcd();
 	_delay_ms(25);
-	TCNT2 = -1; // of TCNT2=0xf6
-	TIMSK |= BIT(6); // T2 overflow interrupt enable, p. 162
-	TCCR2 = 0b00011111; //ext counting, rising
 
+	//set overflow timer 2 -1
+	TCNT2 = -1; // of TCNT2=0xf6
+	
+	TIMSK |= BIT(6); // T2 overflow interrupt enable, p. 162
+
+	TCCR2 = 0b00011111; //ext counting, rising
+	
+	//set port D pin 7 as input
 	DDRD &= ~BIT(7);
 
+	//enable interupts
 	sei();
 	while(1)
 	{
-		
-
 	}
 }
